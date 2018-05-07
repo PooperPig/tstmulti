8bf49d55eb4e3bca53e64216bcb10643c74d0449
Max <mailmaxxx@gmail.com>
2018-05-07 20:32:16 +1000

    Install and use an ambcomponent

--- projects/tradies/src/app/app.module.ts
+++ projects/tradies/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AmbcomponentsModule } from 'ambcomponents';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AmbcomponentsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
