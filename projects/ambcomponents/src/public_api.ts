f91e48e9373789c49d519466dafdc10b2d5fb2f0
Max <mailmaxxx@gmail.com>
2018-05-07 14:26:06 +1000

    Removed service from components package

--- projects/ambcomponents/src/public_api.ts
+++ projects/ambcomponents/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ambcomponents
  */
 
-export * from './lib/ambcomponents.service';
 export * from './lib/ambcomponents.component';
 export * from './lib/ambcomponents.module';
